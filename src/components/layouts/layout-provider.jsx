957203d02ab6f0ab5534e77e63e42740ade777ae
alivolley <alicryptovolley@gmail.com>
2023-07-12 13:59:30 +0330

    sidebar responsived

--- src/components/layouts/layout-provider.jsx
+++ src/components/layouts/layout-provider.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Drawer, useMediaQuery, useTheme } from '@mui/material';
 
 //assets
 import { LayoutProviderStyle } from './layout-provider.style';
@@ -10,14 +11,22 @@ import Navbar from './navbar';
 import SideBar from './sidebar';
 
 const LayoutProvider = () => {
+    const [showSideBar, setShowSideBar] = useState(false);
+
+    const theme = useTheme();
+    const isLaptop = useMediaQuery(theme.breakpoints.up('lg'));
+
     return (
         <>
             <Navbar />
             <LayoutProviderStyle bg={bg}>
-                <SideBar />
+                {isLaptop && <SideBar />}
                 <div className='content'>
                     <Outlet />
                 </div>
+                <Drawer anchor='right' open={showSideBar} onClose={() => setShowSideBar(false)}>
+                    <SideBar />
+                </Drawer>
             </LayoutProviderStyle>
         </>
     );
