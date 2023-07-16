5224171386c56dffd63fc99593f2eed347063f25
kasra-habibbeygi <kasrahabibbeygi@gmail.com>
2023-07-16 17:35:39 +0330

    change some permission

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import VerificationInput from 'react-verification-input';
 
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -14,14 +14,13 @@ const User = createSlice({
             company_name: '',
             company_owner: '',
             role: '',
-            permission: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+            permission: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
         }
     },
     reducers: {
         loginStatushandler: (state, action) => {
             state.isLoggedIn = action.payload;
         },
-
         infoHandler: (state, action) => {
             state.info = action.payload;
         }
